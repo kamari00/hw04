0be9159545b9134722b499797ed40aefc8cc2436
kamari00 <mkalinichenko.007@gmail.com>
2023-10-24 23:19:22 +0200

    hw

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
-
 while True:
     # Введення повідомлення для відправлення серверу
     message = input("Введіть повідомлення: ")
@@ -12,9 +11,11 @@ while True:
     # Відправлення повідомлення серверу
     client_socket.send(message.encode('utf-8'))
 
+
     # Отримання відповіді від сервера
     response = client_socket.recv(1024).decode('utf-8')
     print("Відповідь від сервера: %s" % response)
 
 # Закриття з'єднання
 client_socket.close()
+
